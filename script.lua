44c2a51e8997503ac8a05878c1ec2eb1557ca621
arda <67337599+Kestaneahhohh@users.noreply.github.com>
2021-06-17 16:50:55 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,11 +1,11 @@
  local messages = {
  
- //write your message
+ [1] = "your message"
  
  }
  while true do
      for i, bruh in pairs(messages) do
         wait()
-        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(messages,"LocalPlayer")
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(messages[i],"LocalPlayer")
      end
 end
